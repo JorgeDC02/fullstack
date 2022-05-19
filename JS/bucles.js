6d61b06b5cdd2937cf8bbcd03d25dd0fe9ade4fe
JorgeDC02 <jordi_dc2@hotmail.com>
2022-05-19 14:00:05 +0200

    UPDATE bucles

---
+++ JS/bucles.js
@@ -0,0 +1,47 @@
+ /*******************************************************************
+*Ejercicio 8: escribe un programa que muestre todos los números desde el 1 al 50.
+********************************************************************/
+for(let i = 1; i <= 50; i++){
+    console.log(i);
+}
+
+/*******************************************************************
+*Ejercicio 9: escribe un programa que imprima toda la tabla de multiplicar del 5 (desde 0 hasta 10).
+********************************************************************/
+//let resultado;
+for(let i = 0; i <= 10; i++){
+    //resultado = 5*i;
+    document.write(`5 * ${i} = ${5*i} <br>`);
+}
+
+/*******************************************************************
+*Ejercicio 10: Ejercicio 10: escribe un programa que pida al usuario una palabra (mediante la función prompt) y lo muestre por pantalla 10 veces.
+********************************************************************/
+let palabra = prompt("Ingrese una palabra");
+for(let i = 0 ; i <= 9; i++){
+    document.write(`${i+1} - Se ha ingresado la palabra: ${palabra}<br>`);
+}
+
+/*******************************************************************
+* Ejercicio 11: escribe un programa que pida al usuario un número entero positivo
+*(mediante la función prompt) y muestre por pantalla la cuenta atrás desde ese número
+*hasta cero, utilizando comas como separación.
+********************************************************************/
+var numero2 = prompt("Ingrese un número entero positivo");
+console.log(numero2);
+console.log(typeof parseInt(numero2));
+let entero = parseInt(numero2);
+
+if(numero2 < 0){
+    alert("Ingrese un número entero positivo");
+    numero2 = prompt("Ingrese un número positivo");
+}else if(Number.isInteger(numero2)){
+    alert("Ingrese un número entero");
+    numero2 = prompt("Ingrese un número positivo");
+}else{
+    for(let z = numero2; z >= 0; z--){
+        
+        console.log(`${z},`);
+    }
+}
+
--- JS/dibujos.js
+++ JS/dibujos.js
@@ -1,72 +1,79 @@
+const $canvas = document.querySelector("#canvas"),
+    $btnDescargar = document.querySelector("#btnDescargar"),
+    $btnLimpiar = document.querySelector("#btnLimpiar"),
+    $btnGenerarDocumento = document.querySelector("#btnGenerarDocumento");
+    const contexto = $canvas.getContext("2d");
+    const COLOR_PINCEL = "black";
+    const COLOR_FONDO = "white";
+    const GROSOR = 2;
+    let xAnterior = 0, yAnterior = 0, xActual = 0, yActual = 0;
+    const obtenerXReal = (clientX) => clientX - $canvas.getBoundingClientRect().left;
+    const obtenerYReal = (clientY) => clientY - $canvas.getBoundingClientRect().top;
+    let haComenzadoDibujo = false; // Bandera que indica si el usuario está presionando el botón del mouse sin soltarlo
 
-// Esperamos el contenido del elemento de ventana para hacer las operaciones 
-window.addEventListener('load', ()=>{ 
-		
-	redimensionar(); // Redimensionamos el tamaño del canvas al cargar la Página 
-	document.addEventListener('mousedown', iniciarDibujado); 
-	document.addEventListener('mouseup', detenerDibujado); 
-	document.addEventListener('mousemove', dibujo); 
-	window.addEventListener('resize', redimensionar); 
-}); 
-	
-// Seleccionamos el id del elemento 
-const canvas = document.querySelector('#micanvas'); 
 
-// Contexto del canva para operaciones 2d 
-const ctx = canvas.getContext('2d'); 
-	
-// Redimensionamos el tamaño del canvas según el tamaño de la ventana 
-function redimensionar(){ 
-	ctx.canvas.width = window.innerWidth; 
-	ctx.canvas.height = window.innerHeight; 
-} 
-	
-// Colocamos el curos en su posición inicial 
-let coordenadas = {x:0 , y:0}; 
+const limpiarCanvas = () => {
+    // Colocar color blanco en fondo de canvas
+    contexto.fillStyle = COLOR_FONDO;
+    contexto.fillRect(0, 0, $canvas.width, $canvas.height);
+};
+limpiarCanvas();
+$btnLimpiar.onclick = limpiarCanvas;
 
-// Con esta variable se inicia deshabilitado el dibujado en el Canvas 
-let dibujar = false; 
-	
-// Actualizamos las coordenadas del cursor cuando un evento se dispara
-function obtenerPosicion(event){ 
-	coordenadas.x = event.clientX - canvas.offsetLeft; 
-	coordenadas.y = event.clientY - canvas.offsetTop; 
-} 
+// Escuchar clic del botón para descargar el canvas
+/*$btnDescargar.onclick = () => {
+    const enlace = document.createElement('a');
+    // El título
+    enlace.download = "Firma.png";
+    // Convertir la imagen a Base64 y ponerlo en el enlace
+    enlace.href = $canvas.toDataURL();
+    // Hacer click en él
+    enlace.click();
+};
 
-// Habilitamos el dibujado en el Canvas  
-function iniciarDibujado(event){ 
-	dibujar = true; 
-	obtenerPosicion(event); 
-} 
+window.obtenerImagen = () => {
+    return $canvas.toDataURL();
+};
 
-// Detenemos el dibujado 
-function detenerDibujado(){ 
-	dibujar = false; 
-} 
-	
-function dibujo(event){ 
-	
-	if (!dibujar) return; 
+$btnGenerarDocumento.onclick = () => {
+    window.open("documento.html");
+};*/
 
-	ctx.beginPath(); 
-		
-	ctx.lineWidth = 5; 
+// Lo demás tiene que ver con pintar sobre el canvas en los eventos del mouse
+$canvas.addEventListener("mousedown", evento => {
+    // En este evento solo se ha iniciado el clic, así que dibujamos un punto
+    xAnterior = xActual;
+    yAnterior = yActual;
+    xActual = obtenerXReal(evento.clientX);
+    yActual = obtenerYReal(evento.clientY);
+    contexto.beginPath();
+    contexto.fillStyle = COLOR_PINCEL;
+    contexto.fillRect(xActual, yActual, GROSOR, GROSOR);
+    contexto.closePath();
+    // Y establecemos la bandera
+    haComenzadoDibujo = true;
+});
 
-	// Trazo redondeado 
-	ctx.lineCap = 'round'; 
-		
-	// Color del trazo del dibujo en el Canvas 
-	ctx.strokeStyle = '#5858ef'; 
-		
-	// El cursor para comenzar a dibujar se mueve a esta coordenada 
-	ctx.moveTo(coordenadas.x, coordenadas.y); 
+$canvas.addEventListener("mousemove", (evento) => {
+    if (!haComenzadoDibujo) {
+        return;
+    }
+    // El mouse se está moviendo y el usuario está presionando el botón, así que dibujamos todo
 
-	// La posición del cursor se actualiza a medida que movemos el mouse alrededor del Canvas 
-	obtenerPosicion(event); 
-
-	// Se traza una línea desde el inicio 
-	ctx.lineTo(coordenadas.x , coordenadas.y); 
-		
-	// Dibujamos las líneas  
-	ctx.stroke(); 
-}
+    xAnterior = xActual;
+    yAnterior = yActual;
+    xActual = obtenerXReal(evento.clientX);
+    yActual = obtenerYReal(evento.clientY);
+    contexto.beginPath();
+    contexto.moveTo(xAnterior, yAnterior);
+    contexto.lineTo(xActual, yActual);
+    contexto.strokeStyle = COLOR_PINCEL;
+    contexto.lineWidth = GROSOR;
+    contexto.stroke();
+    contexto.closePath();
+});
+["mouseup", "mouseout"].forEach(nombreDeEvento => {
+    $canvas.addEventListener(nombreDeEvento, () => {
+        haComenzadoDibujo = false;
+    });
+});
