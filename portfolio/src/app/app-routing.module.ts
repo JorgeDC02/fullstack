c979370d0848032caae37110d55d9a8394e1cf1b
JorgeDC02 <jordi_dc2@hotmail.com>
2022-06-03 10:15:22 +0200

    upload proyect angular - porfolio

---
+++ portfolio/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+/**
+ * RUTAS
+ */
+import { Routes } from '@angular/router';
+
+/**
+ * COMPONENTES
+ */
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+
